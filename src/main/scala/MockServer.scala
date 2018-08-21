0dfff6e9a6cb94a2d9480e9b85560e8b7fc38146
Adinath Shirsath <adinathshirsath@AdinathShirsath-Mac.local>
2018-08-20 23:21:18 -0400

    add response based on header and query param

--- src/main/scala/MockServer.scala
+++ src/main/scala/MockServer.scala
@@ -7,9 +7,16 @@ object MockServer extends App {
   override def main(args: Array[String]): Unit = {
     val server = new WireMockServer(options().usingFilesUnderDirectory("src/main/resources"))
     server.start
-    configureFor("localhost",8080)
+    configureFor("localhost", 8080)
+
     stubFor(get(urlEqualTo("/test"))
-        .willReturn(aResponse()
-          .withBodyFile("positions.json")))
+      .willReturn(aResponse()
+        .withBodyFile("positions.json")))
+
+    stubFor(get(urlPathMatching("/positions"))
+      .withQueryParam("groupBy", matching("assets"))
+      .withHeader("x-member-id", matching("23232"))
+      .willReturn(aResponse()
+        .withBodyFile("positions_group_by_assets.json")))
   }
 }
